651f321648083489b1aec0b5c993bf875a46ecc2
LeeeeoLiu <yxliu@ir.hit.edu.cn>
2020-05-28 03:09:31 +0800

    删除多余文字 bug

--- js/delete_title.js
+++ js/delete_title.js
@@ -1,3 +1,3 @@
 var pics_wall = document.getElementById('pics_wall');
-pics_wall.parentNode.removeChild(a.parentNode.children[0])
-pics_wall.parentNode.removeChild(a.parentNode.children[0])
+pics_wall.parentNode.removeChild(pics_wall.parentNode.children[0])
+pics_wall.parentNode.removeChild(pics_wall.parentNode.children[0])
